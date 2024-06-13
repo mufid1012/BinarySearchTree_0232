a21cdffcc8602d8f93409c7a74448a78c838c3da
mufid1012 <mufidghibran1012@gmail.com>
2024-06-13 15:24:57 +0700

    menambahkan void insert dengan string element

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -28,5 +28,16 @@ public:
         ROOT = nullptr; //Initializinf ROOT to null
     }  
 
+     void insert(string element)
+    {
+        Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the new node
+        newNode->info = element; // Assign value to the data field of the new node
+        newNode->leftchild = nullptr; // Make the left child of the new node point tu NULL
+        newNode->rightchild = nullptr; // Make the right child of the new node point tu NULL
+
+    
+
+    }
+
 
 };
